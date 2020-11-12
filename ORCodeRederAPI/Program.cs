8510056a82bf4b06febc39f1933af8b683ee47d9
Likhit Hegu <P10256887@capita.co.uk>
2020-11-12 12:32:17 +0530

    First commit for my test project

---
+++ ORCodeRederAPI/Program.cs
@@ -0,0 +1,44 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace ORCodeRederConsole
+{
+    class Program
+    {
+        /// <summary>
+        /// args[0] is used to pass in file path
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args.Length > 2)
+            {
+                Console.WriteLine("Only 1 parameter is expected which will be file path of the QR code to be scanned");
+                return;
+            }
+            AsyncContext.Run(() => ConvertToData(args[0]));
+            Console.ReadLine();
+        }
+        static async void ConvertToData(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+
+                Console.WriteLine("No file found to be scanned, please check path provided as argument");
+                return;
+            }
+
+            var fileStream = new FileStream(filePath, FileMode.Open);
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                formData.Add(new StreamContent(fileStream), "file", "LHfile");
+                var response = await client.PostAsync("http://api.qrserver.com/v1/read-qr-code/", formData);
+                var qrCodeData = response.Content.ReadAsStringAsync();
+                Console.WriteLine(qrCodeData.Result);
+            }
+        }
+    }
+}
---
+++ QRCodeReader/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace QRCodeReader.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet()]
+        [Route("api/Values/{*filePath}")]
+        public async Task<string> ConvertToData(string filePath)
+        {
+            try
+            {
+                var fileStream = new FileStream(filePath, FileMode.Open);
+                using (var client = new HttpClient())
+                using (var formData = new MultipartFormDataContent())
+                {
+                    formData.Add(new StreamContent(fileStream), "file", "LHfile");
+                    var response = await client.PostAsync("http://api.qrserver.com/v1/read-qr-code/", formData);
+                    var qrCodeData = await response.Content.ReadAsStringAsync();
+                    return qrCodeData.ToString();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                return "File not found, please check file path";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
