3bc7ae36fab96eef11a583bef24f92ffd64ebbd7
Likhit Hegu <P10256887@capita.co.uk>
2020-11-12 13:35:50 +0530

    Final Commit

---
+++ QRCodeReader/Controllers/QRCodeReaderController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace QRCodeReader.Controllers
+{
+    public class QRCodeReaderController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet()]
+        [Route("api/QRCodeReader/{*filePath}")]
+        public async Task<string> ConvertToData(string filePath)
+        {
+            try
+            {
+                var fileStream = new FileStream(filePath, FileMode.Open);
+                using (var client = new HttpClient())
+                using (var formData = new MultipartFormDataContent())
+                {
+                    formData.Add(new StreamContent(fileStream), "file", "LHfile");
+                    var response = await client.PostAsync("http://api.qrserver.com/v1/read-qr-code/", formData);
+                    var qrCodeData = await response.Content.ReadAsStringAsync();
+                    return qrCodeData.ToString();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                return "File not found, please check file path";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- QRCodeReader/Controllers/ValuesController.cs
+++ QRCodeReader/Controllers/ValuesController.cs
@@ -37,32 +37,6 @@ namespace QRCodeReader.Controllers
         public void Delete(int id)
         {
         }
-
-        [HttpGet()]
-        [Route("api/Values/{*filePath}")]
-        public async Task<string> ConvertToData(string filePath)
-        {
-            try
-            {
-                var fileStream = new FileStream(filePath, FileMode.Open);
-                using (var client = new HttpClient())
-                using (var formData = new MultipartFormDataContent())
-                {
-                    formData.Add(new StreamContent(fileStream), "file", "LHfile");
-                    var response = await client.PostAsync("http://api.qrserver.com/v1/read-qr-code/", formData);
-                    var qrCodeData = await response.Content.ReadAsStringAsync();
-                    return qrCodeData.ToString();
-                }
-            }
-            catch (FileNotFoundException ex)
-            {
-                return "File not found, please check file path";
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-        }
+     
     }
 }
